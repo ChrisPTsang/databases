9e33fc3b2c58c68bceca7bef558444f15bda744c
Hack Reactor Students <accounts@hackreactor.com>
2015-07-24 15:33:22 -0700

    Added Schema

--- server/ondras_schema.sql
+++ server/ondras_schema.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE chat;
+CREATE DATABASE chatTest;
 
-USE chat;
+USE chatTest;
 
 
 
@@ -98,36 +98,4 @@ CREATE TABLE `Friends` (
 -- ('','');
 -- INSERT INTO `Friends` (`User`,`UserFriend`) VALUES
 -- ('','');
-
-
-
-
--- CREATE DATABASE chat;
-
--- USE chat;
-
--- CREATE TABLE messages (
---   /* Describe your table here.*/
---   username VARCHAR(32),
---   message TEXT,
---   room VARCHAR(16),
---   posted DATE -- "YYYY-MM-DD"
--- );
-
-
-
--- /* Create other tables and define schemas for them here! */
--- INSERT INTO messages (username, message, room, posted)
---   VALUES ('JohnDoe', 'Here lies a message from John Doe', 'TestRoom', '2015-06-06');
-
--- -- Query to select messages and order by posted date
--- SELECT username, message, room, posted
---   FROM messages
---   ORDER BY posted;
-
-
-
--- /*  Execute this file from the command line by typing:
---  *    mysql -u root < server/schema.sql
---  *  to create the database and the tables.*/
 
