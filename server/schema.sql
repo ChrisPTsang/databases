b8a0d36f81494a00d12b2b118a1574cd4863b758
Hack Reactor Students <accounts@hackreactor.com>
2015-07-24 12:06:56 -0700

    Did some schema stuff

--- server/schema.sql
+++ server/schema.sql
@@ -4,10 +4,22 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  username VARCHAR(32),
+  message TEXT,
+  room VARCHAR(16),
+  posted DATE
 );
 
-/* Create other tables and define schemas for them here! */
 
+
+/* Create other tables and define schemas for them here! */
+INSERT INTO messages (username, message, room, posted)
+  VALUES ('JohnDoe', 'Here lies a message from John Doe', 'TestRoom', GETDATE());
+
+-- Query to select messages and order by posted date
+SELECT username, message, room, posted
+  FROM messages
+  ORDER BY posted;
 
 
 
